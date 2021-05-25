0ada931ec4e77556eb619b622da697295c7c7c49
ianodad <ianodad@gmail.com>
2021-05-25 13:29:23 +0300

    adding apollo

--- index.js
+++ index.js
@@ -7,13 +7,21 @@ import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 
 import {store, persister} from './src/_store/configurestore';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
 
 const ReactNative = () => (
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persister}>
-      <App />
-    </PersistGate>
-  </Provider>
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persister}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </ApolloProvider>
 );
 
 AppRegistry.registerComponent(appName, () => ReactNative);
