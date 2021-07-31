2e2899c24c377e348111cf69f6299757cbdb7120
ianodad <ianodad@gmail.com>
2021-07-31 13:34:26 +0300

    react-native upgrade manual changes

--- App.js
+++ App.js
@@ -1,3 +1,10 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 import AsyncStorage from '@react-native-community/async-storage';
 import {NavigationContainer} from '@react-navigation/native';
 import React, {Component} from 'react';
