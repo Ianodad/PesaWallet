de076e64dec7b0867c4348042608fba6bcb04dfe
ianodad <ianodad@gmail.com>
2022-05-04 19:14:50 +0300

    added working splash screen

--- android/app/src/main/java/com/pesawallet/MainActivity.java
+++ android/app/src/main/java/com/pesawallet/MainActivity.java
@@ -1,7 +1,12 @@
 package com.pesawallet;
+import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
-import android.os.Bundle;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.zoontek.rnbootsplash.RNBootSplash; // <- add this necessary import
+
+
 
 public class MainActivity extends ReactActivity {
 
@@ -16,5 +21,16 @@ public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(null);
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Override
+            protected void loadApp(String appKey) {
+                RNBootSplash.init(MainActivity.this);
+                super.loadApp(appKey);
+            }
+        };
   }
 }
