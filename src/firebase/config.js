c3af0056541ca63e18bbc13e521b455e3a5febe3
ianodad <ianodad@gmail.com>
2022-06-03 18:12:23 +0300

    update gitignore

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -17,20 +17,13 @@ import {
 // console.log(firebase)
 // console.log(settings);
 const firebaseConfig = {
-  apiKey: 'AIzaSyD9rdhS6tsTrwiSDz03j6WA2pOyWknZOac',
-  authDomain: 'pesawallett.firebaseapp.com',
-  projectId: 'pesawallett',
-  storageBucket: 'pesawallett.appspot.com',
-  messagingSenderId: '445455504561',
-  appId: '1:445455504561:web:b86289d6241b174a1b7e90',
-  measurementId: 'G-Z2T4DZ74CM',
-  // apiKey: API_KEY,
-  // authDomain: API_DOMAIN,
-  // projectId: PROJECT_ID,
-  // storageBucket: STORAGE_BUCKET,
-  // messagingSenderId: MESSAGING_SENDER_ID,
-  // appId: APP_ID,
-  // measurementId: MEASUREMENT_ID,
+  apiKey: API_KEY,
+  authDomain: API_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
 };
 
 if (!firebase.apps.length) {
